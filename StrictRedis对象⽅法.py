ef373c8443e09e9a185778a5125dd32fc40417ed
johnderm <18210299021@163.com>
2019-04-22 21:22:27 +0800

    Update StrictRedis对象⽅法.py

--- StrictRedis对象⽅法.py
+++ StrictRedis对象⽅法.py
@@ -2,6 +2,7 @@ from redis import *
 
 
 class Redis_conn(object):
+    #__init__()方法接收python和redis交互中host、port和第几个db
     def __init__(self, host, port, db):
         self.host = host
         self.port = port
@@ -9,10 +10,10 @@ class Redis_conn(object):
 
     def conn(self):
         self.rs = StrictRedis(host=self.host,
-                                    port=self.port,
-                                    db=self.db,
-                                    decode_responses=True
-                                    )
+                              port=self.port,
+                              db=self.db,           # 对于redis返回的每个中文字会显示成为3个字节表示为16进制例如b'\xe5\x93\x88'
+                              decode_responses=True #  这里需要设置decode解码               
+                              )
         return self.rs
 
     def rs_set(self):
@@ -48,7 +49,7 @@ class Redis_conn(object):
 
 
 if __name__ == '__main__':
-    redis = Redis_conn('192.168.100.100', 6379, 2)
+    redis = Redis_conn('192.168.100.100', 6379, 2) #指定连接的redis服务器地址：host端口默认6379，指定数据库第2个（默认是0）
     redis.conn()
     print(redis.host,redis.port,redis.db)
     redis.rs_set()
