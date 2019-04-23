819d8a727e0ad2fe3f5ca9bfffb47fd64681a799
johnderm <18210299021@163.com>
2019-04-23 10:10:04 +0800

    Add files via upload

---
+++ StrictRedis对象⽅法hash类型.py
@@ -0,0 +1,66 @@
+from redis import *
+
+
+class Redis_conn(object):
+    def __init__(self, host, port, db):
+        self.host = host
+        self.port = port
+        self.db = db
+
+    def conn(self):
+        self.rs = StrictRedis(host=self.host,
+                              port=self.port,
+                              db=self.db,           # 使用解码这样中文就不出现这样的
+                              decode_responses=True  # b'\xe8\xb0\xa2\xe9\x80\x8
+                              )
+        return self.rs
+
+    # hash用于存储对象key，对象key结构为属性和属性值
+    # hset key field value
+    def rs_add(self):
+        try:
+            # hmset接收两个参数一个是对象key名，后面的mapping也就是接收类似于字典或者
+            # js对象格式的大括号加键值对{key:value,key1:value1...}
+            result = self.rs.hmset('hash_data', {'name':'peter','age':20})
+            print(result)
+        except Exception as e:
+            print(e)
+
+    def rs_get(self):
+        try:
+            result = self.rs.mget('')
+            # hkey获取hash的对象所有属性
+            result = self.rs.hkeys('hash_data')
+            print(result)
+            # hvals获取hash的所有对象属性值
+            result = self.rs.hvals('hash_data')
+            print(result)
+        except Exception as e:
+            print(e)
+
+    def rs_modify(self):
+        try:
+            result = self.rs.hset('hash_data', 'name', '谢逊')
+            print(result)
+        except Exception as e:
+            print(e)
+
+    def rs_delete(self):
+        try:
+            result = self.rs.hdel('hash_data', 'name')
+            print(result)
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        print('执行完毕')
+
+
+if __name__ == '__main__':
+    redis = Redis_conn('192.168.100.100', 6379, 2)
+    redis.conn()
+    print(redis.host,redis.port,redis.db)
+    redis.rs_add()
+    redis.rs_get()
+    redis.rs_modify()
+    redis.rs_get()
